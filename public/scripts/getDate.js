431086431b7c594c0f13959102902c1a76ce7e2e
pisces65 <guyifan252@163.com>
2021-05-16 16:51:54 +0800

    animate-calendar排序实现

--- public/scripts/getDate.js
+++ public/scripts/getDate.js
@@ -48,15 +48,34 @@ $(document).ready(function(){
     let weekday =$("div.time div:last-child p");
     date.text(today_string);
     weekday.text(today[3]);
+    let anime_items = [];
     $.getJSON("public/bangumi-data/04.json", (bangumidata)=>{
         let animeCalendar = $('.anime-calendar');
-        let anime_weekday = "";
+        //find
         bangumidata.forEach(item => {
             anime_date = UTCtoCN(item.begin);
             if(anime_date[3] == today[3]){
-                let singleAnime = $("<div class='anime-item'><div><p class='jp'>"+anime_date[4]+":"+anime_date[5]+"</p></div><div><a href='"+item.officialSite+"' target='_blank'><p class='jp'>"+item.title+"</p></a></div></div>");
-                animeCalendar.append(singleAnime);
+                item.daytime = anime_date[4]+":"+anime_date[5];
+                // let singleAnime = $("<div class='anime-item'><div><p class='jp'>"+anime_date[4]+":"+anime_date[5]+"</p></div><div><a href='"+item.officialSite+"' target='_blank'><p class='jp'>"+item.title+"</p></a></div></div>");
+                anime_items.push(item);
+            };
+        });
+        //sort
+        anime_items.sort(function(a, b) {
+            var daytimeA = a.daytime;
+            var daytimeB = b.daytime;
+            if (daytimeA < daytimeB) {
+              return -1;
             }
+            if (daytimeA > daytimeB) {
+              return 1;
+            }
+            return 0;
+          });
+          anime_items.forEach(item => {
+            let singleAnime = $("<div class='anime-item'><div><p class='jp'>"+item.daytime+"</p></div><div><a href='"+item.officialSite+"' target='_blank'><p class='jp'>"+item.title+"</p></a></div></div>");
+            animeCalendar.append(singleAnime);
         });
     })
+    
 });
