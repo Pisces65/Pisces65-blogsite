45f3ffa288a053c6c4a6a37397da2aa22106b4ba
pisces65 <guyifan252@163.com>
2021-05-17 20:10:56 +0800

    anime-calendar 中文，排序，日期切换

--- public/scripts/anime-calendar.js
+++ public/scripts/anime-calendar.js
@@ -20,12 +20,12 @@ const toJPWeekday = (eng)=>{
         return "土曜日";
     }
 }
-const UTCtoCN = (dateString)=>{
+const UTCtoCN = (dateString,timezone)=>{
     let d = new Date(dateString);
     let localTime = d.getTime();
     let localOffset = d.getTimezoneOffset() * 60000;
     let utc = localTime + localOffset;
-    let offset = 8;
+    let offset = timezone;
     let calctime = utc + (3600000*offset);
     let cn = new Date(calctime);
     let weekday = cn.toDateString().split(' ')[0];
@@ -39,23 +39,32 @@ const UTCtoCN = (dateString)=>{
     }
     return [year,month,day,weekday,hour,minute]
 }
-let today = UTCtoCN(Date.now());
-let today_string = today.slice(0,3);
-today_string = today_string.join('.');
+const getTitleTranslate = (object)=>{
+    let translated_title = "NOT FOUND";
+    if("zh-Hans" in object){
+        translated_title = object["zh-Hans"][0];
+    }else if("en" in object){
+        translated_title = object["en"][0];
+    }
+    return translated_title
+}
 
-$(document).ready(function(){
-    let reversed = false;
-    let isJP = true;
-    let date =$("div.time div:first-child p");
-    let weekday =$("div.time div:nth-child(2) p");
+const getBangumiData = (reversed, isJP, today)=>{
+    let today_string = today.slice(0,3);
+    today_string = today_string.join('.');
+    let date =$("div.time div:nth-child(2) p");
+    let weekday =$("div.time div:nth-child(3) p");
     date.text(today_string);
     weekday.text(today[3]);
+
+
     let anime_items = [];
+    $(".anime-item").remove();
     let animeCalendar = $('.anime-calendar');
     $.getJSON("public/bangumi-data/04.json", (bangumidata)=>{
         //find
         bangumidata.forEach(item => {
-            anime_date = UTCtoCN(item.begin);
+            let anime_date = UTCtoCN(item.begin,8);
             if(anime_date[3] == today[3]){
                 item.daytime = anime_date[4]+":"+anime_date[5];
                 anime_items.push(item);
@@ -73,51 +82,89 @@ $(document).ready(function(){
             }
             return 0;
         });
-        anime_items.forEach(item => {
-        let singleAnime = $("<div class='anime-item'><div><p class='jp'>"+item.daytime+"</p></div><div><a href='"+item.officialSite+"' target='_blank'><p class='jp'>"+item.title+"</p></a></div></div>");
-        animeCalendar.append(singleAnime);
-        reversed = false;
-        isJP = true;
-        });
+        //display
+        if(!reversed){
+            if(isJP){
+                anime_items.forEach(item => {
+                let singleAnime = $("<div class='anime-item'><div><p class='jp'>"+item.daytime+"</p></div><div><a href='"+item.officialSite+"' target='_blank'><p class='jp'>"+item.title+"</p></a></div></div>");
+                animeCalendar.append(singleAnime);
+                });
+            }else{
+                for(let i=0;i < anime_items.length;i++){
+                    let titleTranslate_Object = anime_items[i].titleTranslate;
+                    let anime_bangumi_name = getTitleTranslate(titleTranslate_Object);
+                    let singleAnime = $("<div class='anime-item'><div><p class='jp'>"+anime_items[i].daytime+"</p></div><div><a href='"+anime_items[i].officialSite+"' target='_blank'><p class='zh'>"+anime_bangumi_name+"</p></a></div></div>");
+                    animeCalendar.append(singleAnime);
+                }
+            }
+        }else{
+            if(isJP){
+                for(let i = anime_items.length-1; i >= 0; i--){
+                    let singleAnime = $("<div class='anime-item'><div><p class='jp'>"+anime_items[i].daytime+"</p></div><div><a href='"+anime_items[i].officialSite+"' target='_blank'><p class='zh'>"+anime_items[i].title+"</p></a></div></div>");
+                    animeCalendar.append(singleAnime);
+                }
+            }else{
+                for(let i = anime_items.length-1; i >= 0; i--){
+                    let titleTranslate_Object = anime_items[i].titleTranslate;
+                    let anime_bangumi_name = getTitleTranslate(titleTranslate_Object);
+                    let singleAnime = $("<div class='anime-item'><div><p class='jp'>"+anime_items[i].daytime+"</p></div><div><a href='"+anime_items[i].officialSite+"' target='_blank'><p class='zh'>"+anime_bangumi_name+"</p></a></div></div>");
+                    animeCalendar.append(singleAnime);
+                }
+            }
+        }
     });
+    return anime_items;
+}
+
+
+$(document).ready(function(){
+    const ONEDAY = 24;
+    let current_zone = 8;
+    let today = UTCtoCN(Date.now(),current_zone);
+    let reversed = false;
+    let isJP = true;
+    let anime_items = [];
+    let animeCalendar = $('.anime-calendar');
+
+    anime_items = getBangumiData(reversed, isJP, today);
     
     $("#anime-sort").click(function(){
+        reversed = !reversed;
+        console.log("now revered?",reversed);
         $('#anime-sort').toggleClass('img-down img-up');
         if($('#anime-sort').hasClass('img-down')){
             $(".anime-item").remove();
             if(isJP){
-                anime_items.forEach(item => {
-                    let singleAnime = $("<div class='anime-item'><div><p class='jp'>"+item.daytime+"</p></div><div><a href='"+item.officialSite+"' target='_blank'><p class='jp'>"+item.title+"</p></a></div></div>");
+                for(let i = 0; i < anime_items.length; i++){
+                    let singleAnime = $("<div class='anime-item'><div><p class='jp'>"+anime_items[i].daytime+"</p></div><div><a href='"+anime_items[i].officialSite+"' target='_blank'><p class='jp'>"+anime_items[i].title+"</p></a></div></div>");
                     animeCalendar.append(singleAnime);
-                });
-                reversed = false;
-                console.log("reversed:",reversed);
+                }
             }else{
-                anime_items.forEach(item => {
-                    let singleAnime = $("<div class='anime-item'><div><p class='zh'>"+item.daytime+"</p></div><div><a href='"+item.officialSite+"' target='_blank'><p class='jp'>"+item.titleTranslate["zh-Hans"][0]+"</p></a></div></div>");
+                for(let i = 0; i < anime_items.length; i++){
+                    let titleTranslate_Object = anime_items[i].titleTranslate;
+                    let anime_bangumi_name = getTitleTranslate(titleTranslate_Object);
+                    let singleAnime = $("<div class='anime-item'><div><p class='jp'>"+anime_items[i].daytime+"</p></div><div><a href='"+anime_items[i].officialSite+"' target='_blank'><p class='zh'>"+anime_bangumi_name+"</p></a></div></div>");
                     animeCalendar.append(singleAnime);
-                });
-                reversed = false;
-                console.log("reversed:",reversed);
+                }
             }
+            // reversed = false;
         }
         if($('#anime-sort').hasClass('img-up')){
             $(".anime-item").remove();
             if(isJP){
-                for(let i = anime_items.length-1;i >=0;i--){
+                for(let i = anime_items.length-1; i >= 0; i--){
                     let singleAnime = $("<div class='anime-item'><div><p class='jp'>"+anime_items[i].daytime+"</p></div><div><a href='"+anime_items[i].officialSite+"' target='_blank'><p class='jp'>"+anime_items[i].title+"</p></a></div></div>");
                     animeCalendar.append(singleAnime);
                 }
-                reversed = true;
-                console.log("reversed:",reversed)
             }else{
-                for(let i = anime_items.length-1;i >=0;i--){
-                    let singleAnime = $("<div class='anime-item'><div><p class='zh'>"+anime_items[i].daytime+"</p></div><div><a href='"+anime_items[i].officialSite+"' target='_blank'><p class='jp'>"+anime_items[i].titleTranslate["zh-Hans"][0]+"</p></a></div></div>");
+                for(let i = anime_items.length-1; i >= 0; i--){
+                    let titleTranslate_Object = anime_items[i].titleTranslate;
+                    let anime_bangumi_name = getTitleTranslate(titleTranslate_Object);
+                    let singleAnime = $("<div class='anime-item'><div><p class='jp'>"+anime_items[i].daytime+"</p></div><div><a href='"+anime_items[i].officialSite+"' target='_blank'><p class='zh'>"+anime_bangumi_name+"</p></a></div></div>");
                     animeCalendar.append(singleAnime);
                 }
-                reversed = true;
-                console.log("reversed:",reversed)
             }
+            // reversed = true;
         }
     })
 
@@ -127,7 +174,7 @@ $(document).ready(function(){
         if(reversed){
             if(isJP){
                 for(let i=0;i<anime_items.length;i++){
-                    let anime_single_name = $(".anime-item:nth-child("+(i+3).toString()+") a p");
+                    let anime_single_name = $(".anime-item:nth-child("+(i+4).toString()+") a p");
                     let anime_bangumi_name = anime_items[anime_items.length-1-i].title;
                     if(anime_bangumi_name){
                         anime_single_name.text(anime_bangumi_name)
@@ -136,8 +183,9 @@ $(document).ready(function(){
                 }
             }else{
                 for(let i=0;i<anime_items.length;i++){
-                    let anime_single_name = $(".anime-item:nth-child("+(i+3).toString()+") a p");
-                    let anime_bangumi_name = anime_items[anime_items.length-1-i].titleTranslate["zh-Hans"][0];
+                    let anime_single_name = $(".anime-item:nth-child("+(i+4).toString()+") a p");
+                    let titleTranslate_Object = anime_items[anime_items.length-1-i].titleTranslate;
+                    let anime_bangumi_name = getTitleTranslate(titleTranslate_Object);
                     if(anime_bangumi_name){
                         anime_single_name.text(anime_bangumi_name);
                         anime_single_name.toggleClass("jp zh");
@@ -147,7 +195,7 @@ $(document).ready(function(){
         }else{
             if(isJP){
                 for(let i=0;i<anime_items.length;i++){
-                    let anime_single_name = $(".anime-item:nth-child("+(i+3).toString()+") a p");
+                    let anime_single_name = $(".anime-item:nth-child("+(i+4).toString()+") a p");
                     let anime_bangumi_name = anime_items[i].title;
                     if(anime_bangumi_name){
                         anime_single_name.text(anime_bangumi_name);
@@ -156,8 +204,9 @@ $(document).ready(function(){
                 }
             }else{
                 for(let i=0;i<anime_items.length;i++){
-                    let anime_single_name = $(".anime-item:nth-child("+(i+3).toString()+") a p");
-                    let anime_bangumi_name = anime_items[i].titleTranslate["zh-Hans"][0];
+                    let anime_single_name = $(".anime-item:nth-child("+(i+4).toString()+") a p");
+                    let titleTranslate_Object = anime_items[i].titleTranslate;
+                    let anime_bangumi_name = getTitleTranslate(titleTranslate_Object);
                     if(anime_bangumi_name){
                         anime_single_name.text(anime_bangumi_name);
                         anime_single_name.toggleClass("jp zh");
@@ -166,5 +215,15 @@ $(document).ready(function(){
             }
         }
     })
-    
+
+    $("#anime-yesterday").click(function(){
+        current_zone = current_zone - ONEDAY;
+        today = UTCtoCN(Date.now(),current_zone);
+        anime_items = getBangumiData(reversed, isJP, today);
+    })
+    $("#anime-tomorrow").click(function(){
+        current_zone = current_zone + ONEDAY;
+        today = UTCtoCN(Date.now(),current_zone);
+        anime_items = getBangumiData(reversed, isJP, today);
+    })
 });
